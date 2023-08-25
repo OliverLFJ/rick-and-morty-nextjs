8e4c8507c39f4b889da2c451921a90ab6ee46107
OliverLFJ <oliverde10021@gmail.com>
2023-08-24 18:26:56 -0600

    Fix Next Config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
+/** @type {import('next').NextConfig} */
 const withOptimizedImages = require('next-optimized-images');
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['rickandmortyapi.com'],
-    plugins: [
-      import('imagemin-mozjpeg').then((imageminMozjpeg) => {
-        return imageminMozjpeg.default({
-          quality: 50,
-        });
-      }),
-    ],
+    domains: ['rickandmortyapi.com']
   },
-  optimizeImages: true,
-  responsive: {
-    adapter: require.resolve('responsive-loader/sharp'),
-  },
-};
+  ...withOptimizedImages({
+    handleImages: ['jpeg', 'png', 'svg'],
+  }),
+}
 
-module.exports = withOptimizedImages(nextConfig);
+module.exports = nextConfig
+c
