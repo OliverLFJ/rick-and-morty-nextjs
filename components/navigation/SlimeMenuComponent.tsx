e8ca24db898f8d6a4121a27177f3f3656f8d4385
OliverLFJ <oliverde10021@gmail.com>
2023-08-24 19:23:17 -0600

    Fix Bugs in Images

--- components/navigation/SlimeMenuComponent.tsx
+++ components/navigation/SlimeMenuComponent.tsx
@@ -2,15 +2,16 @@
 import NavigationMobileComponent from "./NavigationMobileComponent";
 import styles from "./../Component.module.css"
 import { useMenuContext } from "@/contexts/MenuContext";
+import Image from "next/image";
 
 export default function SlimeMenuComponent() {
 
     const { menuState } = useMenuContext()
 
     return (
-        <div className={menuState?styles.menu_movil:styles.menu_movil_show}>
+        <div className={menuState ? styles.menu_movil : styles.menu_movil_show}>
             <NavigationMobileComponent />
-            <img alt="Logo From Rick And Morty" src="/slimeheader.png" className="slime" />
+            <Image alt="Logo From Rick And Morty" src="/slimeheader.png" className="slime" width="0" height="0" sizes="100vw" />
         </div>
     )
 }
